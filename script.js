fec586d654d4f62359114b3703285313effd4f8a
Krishna <libraryismyparadise@gmail.com>
2022-06-24 21:49:31 +0545

    Add dynamic html content for speakers

--- script.js
+++ script.js
@@ -18,3 +18,65 @@ document.querySelectorAll(".nav-link").forEach((n) =>
 );
 
 /*  ---------Dynamic Html for home page ----------*/
+const speakerData = [
+  {
+    speaker_img: "./src/speaker-1.jpeg",
+    speaker_name: "Walt Whitman",
+    speaker_info: "an American poet, essayist and journalist. A humanist",
+    speaker_detail:
+      "he was a part of the transition between transcendentalism and realism. Whitman is among the most influential poets in the American canon",
+  },
+  {
+    speaker_img: "./src/speaker-2.jpeg",
+    speaker_name: "Allen Ginsberg",
+    speaker_info: "an American poet, essayist and writer. A Rebel",
+    speaker_detail:
+      'Ginsberg is best known for his poem "Howl", in which he denounced what he saw as the destructive forces of capitalism and conformity',
+  },
+  {
+    speaker_img: "./src/speaker-1.jpeg",
+    speaker_name: "Walt Whitman",
+    speaker_info: "an American poet, essayist and journalist. A humanist",
+    speaker_detail:
+      "he was a part of the transition between transcendentalism and realism. Whitman is among the most influential poets in the American canon",
+  },
+  {
+    speaker_img: "./src/speaker-2.jpeg",
+    speaker_name: "Allen Ginsberg",
+    speaker_info: "an American poet, essayist and writer. A Rebel",
+    speaker_detail:
+      'Ginsberg is best known for his poem "Howl", in which he denounced what he saw as the destructive forces of capitalism and conformity',
+  },
+  {
+    speaker_img: "./src/speaker-1.jpeg",
+    speaker_name: "Walt Whitman",
+    speaker_info: "an American poet, essayist and journalist. A humanist",
+    speaker_detail:
+      "he was a part of the transition between transcendentalism and realism. Whitman is among the most influential poets in the American canon",
+  },
+  {
+    speaker_img: "./src/speaker-2.jpeg",
+    speaker_name: "Allen Ginsberg",
+    speaker_info: "an American poet, essayist and writer. A Rebel",
+    speaker_detail:
+      'Ginsberg is best known for his poem "Howl", in which he denounced what he saw as the destructive forces of capitalism and conformity',
+  },
+];
+
+let dynamicSpeakerContent = "";
+for (let i = 0; i < speakerData.length; i += 1) {
+  dynamicSpeakerContent = `
+    <div class="speaker-list">
+    <img class="speaker-img" src="${speakerData[i].speaker_img}" alt = "Whalt Whitman" />
+    <div>
+    <p class="speaker-name">${speakerData[i].speaker_name}</p>
+    <p class = "speaker-info">${speakerData[i].speaker_info}</p>
+    <div class = "sm-line">
+    </div>
+    <p class = "speaker-detail">${speakerData[i].speaker_detail}</p>
+    </div>
+    </div>
+  `;
+  document.getElementsByClassName("speaker-list-wrap")[0].innerHTML +=
+    dynamicSpeakerContent;
+}
