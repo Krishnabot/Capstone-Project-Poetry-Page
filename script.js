d25dc80a4c83799753ddcf719835a8a2069b8b21
Krishna <libraryismyparadise@gmail.com>
2022-06-25 15:55:16 +0545

    Fix Lint Errors

--- script.js
+++ script.js
@@ -1,69 +1,67 @@
 /* -------For mobile menu --------*/
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-const navBar = document.querySelector(".nav-bar");
-const navLink = document.querySelector(".nav-link");
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-  navBar.classList.toggle("active");
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+const navBar = document.querySelector('.nav-bar');
+const navLink = document.querySelector('.nav-link');
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+  navBar.classList.toggle('active');
 });
-document.querySelectorAll(".nav-link").forEach((n) =>
-  n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-    navBar.classList.remove("active");
-    navLink.classList.remove("active");
-  })
-);
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+  navBar.classList.remove('active');
+  navLink.classList.remove('active');
+}));
 
 /*  ---------Dynamic Html for home page ----------*/
 const speakerData = [
   {
-    speaker_img: "./src/speaker-1.jpeg",
-    speaker_name: "Walt Whitman",
-    speaker_info: "an American poet, essayist and journalist. A humanist",
+    speaker_img: './src/speaker-1.jpeg',
+    speaker_name: 'Walt Whitman',
+    speaker_info: 'an American poet, essayist and journalist. A humanist',
     speaker_detail:
-      "he was a part of the transition between transcendentalism and realism. Whitman is among the most influential poets in the American canon",
+      'he was a part of the transition between transcendentalism and realism. Whitman is among the most influential poets in the American canon',
   },
   {
-    speaker_img: "./src/speaker-2.jpeg",
-    speaker_name: "Allen Ginsberg",
-    speaker_info: "an American poet, essayist and writer. A Rebel",
-    speaker_detail:
-      'Ginsberg is best known for his poem "Howl", in which he denounced what he saw as the destructive forces of capitalism and conformity',
-  },
-  {
-    speaker_img: "./src/speaker-1.jpeg",
-    speaker_name: "Walt Whitman",
-    speaker_info: "an American poet, essayist and journalist. A humanist",
-    speaker_detail:
-      "he was a part of the transition between transcendentalism and realism. Whitman is among the most influential poets in the American canon",
-  },
-  {
-    speaker_img: "./src/speaker-2.jpeg",
-    speaker_name: "Allen Ginsberg",
-    speaker_info: "an American poet, essayist and writer. A Rebel",
+    speaker_img: './src/speaker-2.jpeg',
+    speaker_name: 'Allen Ginsberg',
+    speaker_info: 'an American poet, essayist and writer. A Rebel',
     speaker_detail:
       'Ginsberg is best known for his poem "Howl", in which he denounced what he saw as the destructive forces of capitalism and conformity',
   },
   {
-    speaker_img: "./src/speaker-1.jpeg",
-    speaker_name: "Walt Whitman",
-    speaker_info: "an American poet, essayist and journalist. A humanist",
+    speaker_img: './src/speaker-1.jpeg',
+    speaker_name: 'Walt Whitman',
+    speaker_info: 'an American poet, essayist and journalist. A humanist',
     speaker_detail:
-      "he was a part of the transition between transcendentalism and realism. Whitman is among the most influential poets in the American canon",
+      'he was a part of the transition between transcendentalism and realism. Whitman is among the most influential poets in the American canon',
   },
   {
-    speaker_img: "./src/speaker-2.jpeg",
-    speaker_name: "Allen Ginsberg",
-    speaker_info: "an American poet, essayist and writer. A Rebel",
+    speaker_img: './src/speaker-2.jpeg',
+    speaker_name: 'Allen Ginsberg',
+    speaker_info: 'an American poet, essayist and writer. A Rebel',
+    speaker_detail:
+      'Ginsberg is best known for his poem "Howl", in which he denounced what he saw as the destructive forces of capitalism and conformity',
+  },
+  {
+    speaker_img: './src/speaker-1.jpeg',
+    speaker_name: 'Walt Whitman',
+    speaker_info: 'an American poet, essayist and journalist. A humanist',
+    speaker_detail:
+      'he was a part of the transition between transcendentalism and realism. Whitman is among the most influential poets in the American canon',
+  },
+  {
+    speaker_img: './src/speaker-2.jpeg',
+    speaker_name: 'Allen Ginsberg',
+    speaker_info: 'an American poet, essayist and writer. A Rebel',
     speaker_detail:
       'Ginsberg is best known for his poem "Howl", in which he denounced what he saw as the destructive forces of capitalism and conformity',
   },
 ];
 
-let dynamicSpeakerContent = "";
+let dynamicSpeakerContent = '';
 for (let i = 0; i < speakerData.length; i += 1) {
   dynamicSpeakerContent = `
     <div class="speaker-list">
@@ -77,34 +75,34 @@ for (let i = 0; i < speakerData.length; i += 1) {
     </div>
     </div>
   `;
-  document.getElementsByClassName("speaker-list-wrap")[0].innerHTML +=
-    dynamicSpeakerContent;
+  document.getElementsByClassName('speaker-list-wrap')[0].innerHTML
+    += dynamicSpeakerContent;
 }
 
 const programData = [
   {
-    program_icon: "./src/program-intro-icon.png",
-    program_event: "Introduction",
-    event_detail: "Introduction about the event and the poets",
+    program_icon: './src/program-intro-icon.png',
+    program_event: 'Introduction',
+    event_detail: 'Introduction about the event and the poets',
   },
   {
-    program_icon: "./src/program-exibition-icon.png",
-    program_event: "Exibition",
+    program_icon: './src/program-exibition-icon.png',
+    program_event: 'Exibition',
     event_detail:
-      "Informative session about invited poets works and publication",
+      'Informative session about invited poets works and publication',
   },
   {
-    program_icon: "./src/program-workshop-icon.png",
-    program_event: "Workshop",
-    event_detail: "Try express your ideas.",
+    program_icon: './src/program-workshop-icon.png',
+    program_event: 'Workshop',
+    event_detail: 'Try express your ideas.',
   },
   {
-    program_icon: "./src/program-pannel-discussion-icon.png",
-    program_event: "Pannel discussion",
-    event_detail: "Discussion about contemporary poetry and its influence",
+    program_icon: './src/program-pannel-discussion-icon.png',
+    program_event: 'Pannel discussion',
+    event_detail: 'Discussion about contemporary poetry and its influence',
   },
 ];
-let dynamicProgramContent = "";
+let dynamicProgramContent = '';
 for (let i = 0; i < programData.length; i += 1) {
   dynamicProgramContent = `
                      <div class="program-info">
@@ -113,6 +111,6 @@ for (let i = 0; i < programData.length; i += 1) {
                     <p>${programData[i].event_detail}</p>
                     </div>
   `;
-  document.getElementsByClassName("program-info-wrap")[0].innerHTML +=
-    dynamicProgramContent;
+  document.getElementsByClassName('program-info-wrap')[0].innerHTML
+    += dynamicProgramContent;
 }
